b34b1de401ae0f13d592dc5748312f58002673d7
reyfuu <audinathanael@gmail.com>
2026-06-25 12:10:35 +0700

    tambah sql & skills

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * Base controller untuk semua HTTP controller di portal SKINKU B2B.
+ *
+ * Kelas ini berfungsi sebagai parent class yang mengumpulkan
+ * behaviour bersama (middleware, helper, dll) di masa mendatang.
+ * Saat ini bersifat kosong sesuai konvensi Laravel.
+ */
 abstract class Controller
 {
     //
